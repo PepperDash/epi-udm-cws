81955ca96037d37a22bd558989e899934a7075e4
Aviv Cohn <acohn@pepperdash.com>
2025-12-10 20:46:11 -0500

    fix: Pdu-UdmCws-Lib now has files correctly registered

---
+++ Pdu-UdmCws-Epi/UdmCwsHandler.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System.Collections.Generic;
+using PepperDash.Essentials.Core;
+
+namespace PepperDash.Essentials.Plugin.UdmCws
+{
+    public class UdmCwsHandler : IUdmApi
+    {
+
+        
+
+        public void SetDeviceProperty(DeviceKeys key, DeviceStatus device)
+        {
+
+            var temp = PepperDash.Essentials.Core.DeviceManager.GetDeviceForKey(key);
+            if (_roomResponse.Status.Devices == null)
+            {
+                _roomResponse.Status.Devices = new Dictionary<string, DeviceStatus>();
+            }
+            if(_roomResponse.Status.Devices.ContainsKey(key.ToString()))
+            {
+                _roomResponse.Status.Devices[key.ToString()] = device;
+                return;
+            }
+            _roomResponse.Status.Devices.Add(key.ToString(), device);
+        }
+
+        /// <summary>
+        /// Gets the current room response object
+        /// </summary>
+       
+    }
+}
--- Pdu-UdmCws-Epi/UdmCwsStateManager.cs
+++ Pdu-UdmCws-Epi/UdmCwsStateManager.cs
@@ -4,18 +4,14 @@ using PepperDash.Essentials.Core;
 
 namespace PepperDash.Essentials.Plugin.UdmCws
 {
-    public class UdmCwsStateManager
-        : EssentialsDevice
+    public class UdmCwsStateManager : EssentialsDevice
     {
-        /// <summary>
-        /// The UDM CWS handler instance that manages room response data
-        /// </summary>
-        public UdmCwsHandler Handler { get; private set; }
-
+        public RoomResponse State { get; private set; }
         public UdmCwsStateManager(string key) : base(key)
         {
-            Handler = new UdmCwsHandler();
-            Handler.Initialize();
+           State = new RoomResponse();
         }
+
+        public RoomResponse GetState() => State;
     }
 }
